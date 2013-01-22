0a9b4232c15fc35492e27f47a553e2bc9789e1a3
Christopher Toth <q.alpha@gmail.com>
2013-01-22 04:51:54 -0700

    Initial commit, Magic, from the Author of SQLAlchemy's blog post

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+__version__ = 0.1
+__doc__ = """Magic! A thin wrapper over SQLAlchemy to make it a bit more friendly"""
+
+setup(
+ name = "db_magic",
+ version = __version__,
+ description = __doc__,
+ py_modules = ["db_magic"],
+ install_requires = [
+  'sqlalchemy',
+ ],
+ classifiers = [
+  'Development Status :: 3 - Alpha',
+  'Intended Audience :: Developers',
+  'Programming Language :: Python',
+  'Topic :: Software Development :: Libraries',
+ ],
+)
