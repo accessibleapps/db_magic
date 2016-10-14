720ef4182a7df5601570d7fd40fab66b827fd213
Q <q@q-continuum.net>
2016-10-14 00:18:43 -0600

    Upgrade from the current version, not from version 1. Also, wrap each upgrade in its own transaction

--- db_magic/schema_upgrades.py
+++ db_magic/schema_upgrades.py
@@ -9,7 +9,9 @@ def perform_upgrade(session, upgrades=None, schema_version=0):
  logger.info("Current schema version: %d, desired schema version: %d" % (current_version, schema_version))
  if current_version >= schema_version:
   return
- for version in xrange(1, schema_version + 1):
+ for version in xrange(current_version + 1, schema_version + 1):
+  session.begin(subtransactions=True)
+  logger.info("Applying schema version %d" % version)
   try:
    for item in upgrades[version]:
     if callable(item):
@@ -20,7 +22,9 @@ def perform_upgrade(session, upgrades=None, schema_version=0):
    logger.exception("failed upgrading to version %d" % version)
    raise
    break
-  connection.execute('pragma user_version=%d' % version)
+   session.commit()
+  session.execute('pragma user_version=%d' % version)
+  session.commit()
   logger.info("upgraded to schema version %d" % version)
  else:
   logger.warning("No upgrades available to fulfill upgrade request")
